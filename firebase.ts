f9ae8327821186b79796bf0c5267090d250b21ca
Mohd Junaid Pasha <mdjunadepasha@gmail.com>
2026-01-08 19:54:39 +0530

    chore: prep for vercel deploy

--- firebase.ts
+++ firebase.ts
@@ -1,16 +1,17 @@
+/// <reference types="vite/client" />
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAEegHswlx_fuldc1M3VsJNdmBQrJciSVw",
-  authDomain: "foodhelp-53b4d.firebaseapp.com",
-  projectId: "foodhelp-53b4d",
-  storageBucket: "foodhelp-53b4d.firebasestorage.app",
-  messagingSenderId: "393013772236",
-  appId: "1:393013772236:web:4eeb9bbb89fd26529e2769",
-  measurementId: "G-TM8BDBTM2G"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
